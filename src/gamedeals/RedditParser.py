89c8a63180a9ac4134b19204ceb9bb0df14dd1e9
Julius Dehner <julius.dehner@yahoo.de>
2018-11-24 19:21:29 +0100

    Add ini_parser

--- src/gamedeals/RedditParser.py
+++
@@ -1,6 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-reddit_page = requests.get("https://old.reddit.com/r/GameDeals/")
-reddit_soup = BeautifulSoup(reddit_page.content, 'html.parser')
-print(reddit_soup.prettify())
