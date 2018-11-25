73d2ae4802566f25f7f281fa348336440d70ed95
Minmo <maroboro.mike@gmail.com>
2018-11-25 17:33:00 +0100

    Basic implementation

--- src/gamedeals/RedditParser.py
+++ src/gamedeals/RedditParser.py
@@ -1,4 +1,17 @@
+import re
 import praw
 
+from src.gamedeals.FanaticalHandler import FanaticalHandler
+
+fanatical_regex = '(?:https?://)?(?:www.)?fanatical.com/[\w|/|+|&|%|-]*'
+
 reddit = praw.Reddit('game_deals_bot')
-print(reddit.user.me())
+for submission in reddit.subreddit('GameDeals').new(limit=50):
+
+    if 'reddit' in submission.url:
+        fanatical_urls = re.findall(fanatical_regex, submission.selftext)
+    else:
+        if 'fanatical' in submission.url:
+            fanatical_handler = FanaticalHandler
+            fanatical_handler.get_page(submission.url)
+
