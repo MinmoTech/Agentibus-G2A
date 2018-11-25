da1383f47db8497593fbcabd25e8d7469378caf0
Minmo <maroboro.mike@gmail.com>
2018-11-25 17:11:43 +0100

    Implement SteamHandler

---
+++ src/gamedeals/SteamHandler.py
@@ -0,0 +1,22 @@
+from decimal import Decimal
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+
+class SteamHandler:
+    def __init__(self, driver: webdriver.Chrome):
+        self.driver = driver
+
+    def get_game_review_number(self, game_name):
+        driver = self.driver
+        driver.get('https://store.steampowered.com/')
+        search_box = driver.find_element_by_id('store_nav_search_term')
+        search_box.send_keys(game_name)
+        search_box.send_keys(Keys.RETURN)
+        search_result_container = driver.find_element_by_id('search_result_container')
+        search_result_container.find_element_by_tag_name('a').click()
+        user_reviews_count = driver.find_element_by_xpath("//meta[@itemprop='reviewCount']").get_attribute('content')
+        driver.quit()
+        return user_reviews_count
