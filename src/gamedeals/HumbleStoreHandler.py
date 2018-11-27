b6acce9b0caee5a9a0a12f5991f06483b75d71a8
Minmo <maroboro.mike@gmail.com>
2018-11-27 17:35:19 +0100

    Include age check, properly format price

--- src/gamedeals/HumbleStoreHandler.py
+++ src/gamedeals/HumbleStoreHandler.py
@@ -1,6 +1,7 @@
 from decimal import Decimal
 
 from selenium import webdriver
+from selenium.webdriver.support.select import Select
 
 
 class G2AHandler:
@@ -11,9 +12,18 @@ class G2AHandler:
     def get_game_name(self):
         driver = self.driver
         driver.get(self.url)
+        self.__do_age_check(driver)
         return driver.find_element_by_class_name('human_name-view').text
 
     def get_sale_price(self):
         driver = self.driver
         driver.get(self.url)
-        return Decimal(driver.find_element_by_class_name('current-price').text)
+        self.__do_age_check(driver)
+        return Decimal(driver.find_element_by_class_name('current-price').text.replace('€', '').replace(',', '.'))
+
+    @staticmethod
+    def __do_age_check(driver: webdriver.Chrome):
+        if 'Content in this product may not be appropriate for all ages.' in driver.page_source:
+            select = Select(driver.find_element_by_class_name('js-selection-year'))
+            select.select_by_value('1990')
+            driver.find_element_by_class_name('js-submit-button').click()
