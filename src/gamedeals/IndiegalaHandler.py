71df218765b8f4aacf617ac205e37f955871681a
Minmo <maroboro.mike@gmail.com>
2018-11-27 17:06:36 +0100

    Add IndiegalaHandler

---
+++ src/gamedeals/IndiegalaHandler.py
@@ -0,0 +1,36 @@
+from decimal import Decimal
+
+from selenium import webdriver
+
+
+class IndiegalaHandler:
+    def __init__(self, indiegala_driver: webdriver.Chrome, indiegala_url: str):
+        self.driver = indiegala_driver
+        self.indiegala_url = indiegala_url
+
+    def get_game_name(self):
+        driver = self.driver
+        url = self.indiegala_url
+        driver.get(url)
+        if 'crackerjack' not in driver.page_source:
+            return driver.find_element_by_tag_name('h1').text
+        else:
+            return self.__get_game_name_crackerjack(driver)
+
+    @staticmethod
+    def __get_game_name_crackerjack(driver: webdriver.Chrome):
+        game_info = driver.find_element_by_class_name('game-info')
+        return game_info.find_element_by_class_name('title').text
+
+    def get_sale_price(self):
+        driver = self.driver
+        driver.get(self.indiegala_url)
+        if 'crackerjack' not in driver.page_source:
+            sale_price = driver.find_element_by_class_name('current-price').text
+            return Decimal(sale_price.replace('€', ''))
+        else:
+            return self.__get_sale_price_crackerjack(driver)
+
+    @staticmethod
+    def __get_sale_price_crackerjack(driver: webdriver.Chrome):
+        return Decimal(driver.find_element_by_id('price').text)
