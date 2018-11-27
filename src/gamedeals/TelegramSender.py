c2623b2fba8dd08ad8e11ebb774f66da4ab498b4
Minmo <maroboro.mike@gmail.com>
2018-11-27 15:01:50 +0100

    Add TelegramSender + example config.ini,  readd ini_parser

---
+++ src/gamedeals/TelegramSender.py
@@ -0,0 +1,14 @@
+import telegram
+
+from src.gamedeals import ini_parser
+
+
+class TelegramSender:
+    def __init__(self):
+        self.bot = telegram.Bot(ini_parser.get_telegram_token())
+        self.MAX_MESSAGE_LENGTH = 4098
+
+    def send_message(self, message: str):
+        self.bot.send_message(ini_parser.get_telegram_chat_id(), message[:self.MAX_MESSAGE_LENGTH])
+        if len(message) > 0:
+            message[self.MAX_MESSAGE_LENGTH:]
