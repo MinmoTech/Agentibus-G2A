f2461f4caba665265622fc1e62dd7aff10d91f72
Minmo <maroboro.mike@gmail.com>
2018-11-28 14:26:46 +0100

    Add ini parsing for net profit percentage

--- src/gamedeals/ini_parser.py
+++ src/gamedeals/ini_parser.py
@@ -11,3 +11,9 @@ def get_telegram_chat_id():
     parser = configparser.ConfigParser()
     parser.read('./resources/configuration.ini')
     return parser['telegram']['chat_id']
+
+
+def get_net_profit_percentage():
+    parser = configparser.ConfigParser()
+    parser.read('./resources/configuration.ini')
+    return parser['default']['net_profit_percentage']
