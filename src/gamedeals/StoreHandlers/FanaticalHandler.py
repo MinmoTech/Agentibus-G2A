bf286eb7b9f14d6d38e0eb5708ca60f792a8f142
Minmo <maroboro.mike@gmail.com>
2018-12-04 17:30:09 +0100

    Add fanatical star deal option

--- src/gamedeals/StoreHandlers/FanaticalHandler.py
+++ src/gamedeals/StoreHandlers/FanaticalHandler.py
@@ -1,32 +1,28 @@
 from decimal import Decimal
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions
 from selenium import webdriver
-from selenium.webdriver.support.wait import WebDriverWait
 
 
 class FanaticalHandler:
     def __init__(self, fanatical_driver: webdriver.Chrome, fanatical_url: str):
         self.driver = fanatical_driver
-        self.fanatical_url = fanatical_url
+        self.url = fanatical_url
 
     def get_game_name(self):
-        fanatical_driver = self.driver
-        fanatical_driver.get(self.fanatical_url)
-        WebDriverWait(fanatical_driver, 800).until(
-            expected_conditions.visibility_of_element_located((By.TAG_NAME, 'h1'))
-        )
-        game_name = fanatical_driver.find_element_by_tag_name('h1').text
+        driver = self.driver
+        driver.get(self.url)
+        game_name = driver.find_element_by_tag_name('h1').text
         return game_name
 
     def get_sale_price(self):
-        fanatical_driver = self.driver
-        fanatical_driver.get(self.fanatical_url)
-        WebDriverWait(fanatical_driver, 800).until(
-            expected_conditions.visibility_of_element_located((By.TAG_NAME, 'h4'))
-        )
-        sale_price = fanatical_driver.find_element_by_tag_name('h4').find_element_by_tag_name(
-            'span').find_element_by_tag_name(
-            'b').find_element_by_tag_name('span').find_element_by_tag_name('span').text
-        sale_price = Decimal(sale_price.replace('€', ''))
-        return sale_price
+        driver = self.driver
+        driver.get(self.url)
+        if not driver.find_elements_by_class_name("stardeal-extras-container"):
+            sale_price = driver.find_element_by_tag_name('h4').find_element_by_tag_name(
+                'span').find_element_by_tag_name(
+                'b').find_element_by_tag_name('span').find_element_by_tag_name('span').text
+            sale_price = Decimal(sale_price.replace('€', ''))
+            return sale_price
+        else:
+            promo_div = driver.find_element_by_class_name("promo-price")
+            sale_price = promo_div.find_element_by_tag_name('span').find_element_by_class_name('span').text
+            return Decimal(sale_price.replace('€', ''))
