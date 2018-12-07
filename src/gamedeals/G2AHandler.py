99fe31a60517ab2c0f502d0e9d220b707241ae52
Julius Dehner <julius.dehner@yahoo.de>
2018-12-08 00:44:43 +0100

    Remove class architecture from G2AHandler

--- src/gamedeals/G2AHandler.py
+++ src/gamedeals/G2AHandler.py
@@ -13,80 +13,76 @@ from selenium.webdriver.support.wait import WebDriverWait
 from src.gamedeals.SteamHandler import SteamHandler
 
 
-class G2AHandler:
-    def __init__(self, driver: webdriver.Chrome):
-        self.driver = driver
-        self.logger = logging.getLogger()
+def lookup_price_of(game_name: str, driver: webdriver.Chrome):
+    steam_review = _get_steam_review_count(game_name)
+    driver.get('https://www.g2a.com/')
+    with suppress(NoSuchElementException):
+        modal_options_buttons = driver.find_element_by_class_name('modal-options__buttons')
+        cookie_confirm_button = modal_options_buttons.find_element_by_class_name('btn-primary')
+        cookie_confirm_button.click()
+    search_bar_parent = driver.find_element_by_class_name('topbar-search-form')
+    search_bar = search_bar_parent.find_element_by_tag_name('input')
+    search_query = game_name + ' Key Global'
+    actions_send_keys(driver, search_bar, search_query)
+    search_bar.send_keys(Keys.RETURN)
+    product_grids = driver.find_elements_by_class_name('products-grid__item')
+    for product_grid in product_grids:
+        if _find_proper_card(product_grid, game_name, search_query):
+            with suppress(TimeoutException, NoSuchElementException):
+                WebDriverWait(driver, 10).until(
+                    expected_conditions.visibility_of_element_located((By.CLASS_NAME, 'expander__button'))
+                )
+                offer_expander_button = driver.find_element_by_class_name('expander__button')
+                offer_expander_button.click()
+            offers = driver.find_elements_by_class_name('offer')
+            for offer in offers:
+                rating_count = _get_g2a_rating_count(offer)
+                if steam_review < 500:
+                    return _get_price(offer)
+                if rating_count > 1000:
+                    return _get_price(offer)
 
-    def lookup_price_of(self, game_name: str):
-        driver = self.driver
-        steam_review = self._get_steam_review_count(game_name)
-        driver.get('https://www.g2a.com/')
-        with suppress(NoSuchElementException):
-            modal_options_buttons = driver.find_element_by_class_name('modal-options__buttons')
-            cookie_confirm_button = modal_options_buttons.find_element_by_class_name('btn-primary')
-            cookie_confirm_button.click()
-        search_bar_parent = driver.find_element_by_class_name('topbar-search-form')
-        search_bar = search_bar_parent.find_element_by_tag_name('input')
-        search_query = game_name + ' Key Global'
-        actions_send_keys(driver, search_bar, search_query)
-        search_bar.send_keys(Keys.RETURN)
-        product_grids = driver.find_elements_by_class_name('products-grid__item')
-        for product_grid in product_grids:
-            if self._find_proper_card(product_grid, game_name, search_query):
-                with suppress(TimeoutException, NoSuchElementException):
-                    WebDriverWait(driver, 10).until(
-                        expected_conditions.visibility_of_element_located((By.CLASS_NAME, 'expander__button'))
-                    )
-                    offer_expander_button = driver.find_element_by_class_name('expander__button')
-                    offer_expander_button.click()
-                offers = driver.find_elements_by_class_name('offer')
-                for offer in offers:
-                    rating_count = self._get_g2a_rating_count(offer)
-                    if steam_review < 500:
-                        return self._get_price(offer)
-                    if rating_count > 1000:
-                        return self._get_price(offer)
 
-    def _find_proper_card(self, product_grid, game_name: str, search_query: str):
-        card_wrapper = product_grid.find_element_by_class_name('card-wrapper')
-        card_title_element = card_wrapper.find_element_by_class_name('Card__title')
-        card_title = card_title_element.find_element_by_tag_name('a').text
-        words_of_game_name = game_name.split()
-        self.logger.info(f"Comparing original game title ({search_query}) to g2a game title ({card_title})")
-        if all(x in card_title for x in words_of_game_name) and len(search_query) + 1 >= len(card_title):
-            self.logger.info("Success!")
-            card_wrapper.click()
-            return True
-        else:
-            return False
+def _find_proper_card(product_grid, game_name: str, search_query: str):
+    logger = logging.getLogger()
+    card_wrapper = product_grid.find_element_by_class_name('card-wrapper')
+    card_title_element = card_wrapper.find_element_by_class_name('Card__title')
+    card_title = card_title_element.find_element_by_tag_name('a').text
+    words_of_game_name = game_name.split()
+    logger.info(f"Comparing original game title ({search_query}) to g2a game title ({card_title})")
+    if all(x in card_title for x in words_of_game_name) and len(search_query) + 1 >= len(card_title):
+        logger.info("Success!")
+        card_wrapper.click()
+        return True
+    else:
+        return False
 
-    @staticmethod
-    def _get_g2a_rating_count(offer):
-        rating_count_element = offer.find_element_by_class_name('rating-data')
-        seller_info_percent = rating_count_element.find_element_by_class_name('seller-info__percent')
-        separator = rating_count_element.find_element_by_class_name('separator')
-        rating_count_dirty = rating_count_element.text
-        return int(rating_count_dirty.replace(seller_info_percent.text, '').replace(separator.text, ''))
 
-    @staticmethod
-    def _get_steam_review_count(game_name):
-        options = webdriver.ChromeOptions()
-        options.add_argument('headless')
-        driver = webdriver.Chrome(options=options)
-        driver.set_window_size(1800, 1070)
-        driver.implicitly_wait(1)
-        steam_handler = SteamHandler(driver)
-        review_number = steam_handler.get_game_review_number(game_name)
-        driver.quit()
-        return review_number
+def _get_g2a_rating_count(offer):
+    rating_count_element = offer.find_element_by_class_name('rating-data')
+    seller_info_percent = rating_count_element.find_element_by_class_name('seller-info__percent')
+    separator = rating_count_element.find_element_by_class_name('separator')
+    rating_count_dirty = rating_count_element.text
+    return int(rating_count_dirty.replace(seller_info_percent.text, '').replace(separator.text, ''))
 
-    @staticmethod
-    def _get_price(offer):
-        price_element = offer.find_element_by_class_name('price')
-        currency = price_element.find_element_by_class_name('price__currency')
-        price = price_element.text.replace(currency.text, '')
-        return Decimal(price)
+
+def _get_steam_review_count(game_name):
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    driver = webdriver.Chrome(options=options)
+    driver.set_window_size(1800, 1070)
+    driver.implicitly_wait(1)
+    steam_handler = SteamHandler(driver)
+    review_number = steam_handler.get_game_review_number(game_name)
+    driver.quit()
+    return review_number
+
+
+def _get_price(offer):
+    price_element = offer.find_element_by_class_name('price')
+    currency = price_element.find_element_by_class_name('price__currency')
+    price = price_element.text.replace(currency.text, '')
+    return Decimal(price)
 
 
 def actions_click_element(driver: webdriver.Chrome, element):
