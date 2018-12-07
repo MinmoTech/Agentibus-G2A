4fd03407fd2e44157484f9b7320f19566de00d93
Minmo <maroboro.mike@gmail.com>
2018-12-07 23:33:11 +0100

    Replace incorrect dunders with single underscores

--- src/gamedeals/StoreHandlers/HumbleStoreHandler.py
+++ src/gamedeals/StoreHandlers/HumbleStoreHandler.py
@@ -15,17 +15,17 @@ class HumbleStoreHandler:
     def get_game_name(self):
         driver = self.driver
         driver.get(self.url)
-        self.__do_age_check(driver)
+        self._do_age_check(driver)
         return driver.find_element_by_class_name('human_name-view').text
 
     def get_sale_price(self):
         driver = self.driver
         driver.get(self.url)
-        self.__do_age_check(driver)
+        self._do_age_check(driver)
         return Decimal(driver.find_element_by_class_name('current-price').text.replace('€', '').replace(',', '.'))
 
     @staticmethod
-    def __do_age_check(driver: webdriver.Chrome):
+    def _do_age_check(driver: webdriver.Chrome):
         if 'Content in this product may not be appropriate for all ages.' in driver.page_source:
             select = Select(driver.find_element_by_class_name('js-selection-year'))
             select.select_by_value('1990')
