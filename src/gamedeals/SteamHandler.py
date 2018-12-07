abe22ee59e1d4d277c6f8ecae26fe779b8573c81
Minmo <maroboro.mike@gmail.com>
2018-12-08 00:46:23 +0100

    Remove class architecture from SteamHandler

--- src/gamedeals/SteamHandler.py
+++ src/gamedeals/SteamHandler.py
@@ -4,19 +4,14 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-class SteamHandler:
-    def __init__(self, driver: webdriver.Chrome):
-        self.driver = driver
-        self.logger = logging.getLogger()
-
-    def get_game_review_number(self, game_name):
-        driver = self.driver
-        driver.get('https://store.steampowered.com/')
-        search_box = driver.find_element_by_id('store_nav_search_term')
-        search_box.send_keys(game_name)
-        search_box.send_keys(Keys.RETURN)
-        search_result_container = driver.find_element_by_id('search_result_container')
-        search_result_container.find_element_by_tag_name('a').click()
-        user_reviews_count = driver.find_element_by_xpath("//meta[@itemprop='reviewCount']").get_attribute('content')
-        self.logger.info(f'Steam reviews of {game_name}: {user_reviews_count}')
-        return int(user_reviews_count)
+def get_game_review_number(game_name, driver: webdriver.Chrome):
+    logger = logging.getLogger()
+    driver.get('https://store.steampowered.com/')
+    search_box = driver.find_element_by_id('store_nav_search_term')
+    search_box.send_keys(game_name)
+    search_box.send_keys(Keys.RETURN)
+    search_result_container = driver.find_element_by_id('search_result_container')
+    search_result_container.find_element_by_tag_name('a').click()
+    user_reviews_count = driver.find_element_by_xpath("//meta[@itemprop='reviewCount']").get_attribute('content')
+    logger.info(f'Steam reviews of {game_name}: {user_reviews_count}')
+    return int(user_reviews_count)
