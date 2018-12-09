a74c9a89006f1432d49100fec2f5611eb63fecfe
Julius Dehner <julius.dehner@yahoo.de>
2018-12-09 12:46:31 +0100

    Fix wrong byte count

--- src/gamedeals/TelegramSender.py
+++ src/gamedeals/TelegramSender.py
@@ -6,7 +6,7 @@ from src.gamedeals import ini_parser
 class TelegramSender:
     def __init__(self):
         self.bot = telegram.Bot(ini_parser.get_telegram_token())
-        self.MAX_MESSAGE_LENGTH = 4098
+        self.MAX_MESSAGE_LENGTH = 4096
 
     def send_message(self, message: str):
         self.bot.send_message(ini_parser.get_telegram_chat_id(), message[:self.MAX_MESSAGE_LENGTH])
