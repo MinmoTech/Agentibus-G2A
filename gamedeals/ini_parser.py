a856a9db763a2f837d83b69f49519a042d34cd39
Julius Dehner <julius.dehner@yahoo.de>
2018-12-18 20:10:20 +0100

    Move resources

--- gamedeals/ini_parser.py
+++ gamedeals/ini_parser.py
@@ -3,17 +3,17 @@ import configparser
 
 def get_telegram_token():
     parser = configparser.ConfigParser()
-    parser.read('./resources/configuration.ini')
+    parser.read('../resources/configuration.ini')
     return parser['telegram']['token']
 
 
 def get_telegram_chat_id():
     parser = configparser.ConfigParser()
-    parser.read('./resources/configuration.ini')
+    parser.read('../resources/configuration.ini')
     return parser['telegram']['chat_id']
 
 
 def get_net_profit_percentage():
     parser = configparser.ConfigParser()
-    parser.read('./resources/configuration.ini')
+    parser.read('../resources/configuration.ini')
     return parser['default']['net_profit_percentage']
