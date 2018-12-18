3f67ecbde141fb5e069b179d9b8dc29c398229c5
Julius Dehner <julius.dehner@yahoo.de>
2018-12-18 20:53:17 +0100

    Set resources in env variable

--- agentibus/ini_parser.py
+++ agentibus/ini_parser.py
@@ -1,19 +1,20 @@
 import configparser
+import os
 
 
 def get_telegram_token():
     parser = configparser.ConfigParser()
-    parser.read('../resources/configuration.ini')
+    parser.read(f"{os.environ['AGENTIBUS_RESOURCES']}/configuration.ini")
     return parser['telegram']['token']
 
 
 def get_telegram_chat_id():
     parser = configparser.ConfigParser()
-    parser.read('../resources/configuration.ini')
+    parser.read(f"{os.environ['AGENTIBUS_RESOURCES']}/configuration.ini")
     return parser['telegram']['chat_id']
 
 
 def get_net_profit_percentage():
     parser = configparser.ConfigParser()
-    parser.read('../resources/configuration.ini')
+    parser.read(f"{os.environ['AGENTIBUS_RESOURCES']}/configuration.ini")
     return parser['default']['net_profit_percentage']
