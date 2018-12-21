cebc511b675a16724c7dd62ca29708ee64a9ff82
Minmo <maroboro.mike@gmail.com>
2018-12-21 21:50:53 +0100

    Bugfixes

--- agentibus/StoreHandlers/FanaticalHandler.py
+++ agentibus/StoreHandlers/FanaticalHandler.py
@@ -16,15 +16,17 @@ def set_game_data(driver, game: Game):
     if driver.find_elements_by_class_name("stardeal-extras-container"):
         game.sale_price = _get_stardeal_price()
     else:
-        game.sale_price = _get_regular_sale_price(driver)
+        try:
+            game.sale_price = _get_regular_sale_price(driver)
+        except NoSuchElementException:
+            logging.getLogger().error(f'Could not get proper info at {game.url}')
     game.review_count = SteamHandler.get_game_review_number(game.name, driver)
     game.g2a_price = G2AHandler.get_price_of(game, driver)
     Product.set_game_meta_data(game)
 
 
 def _get_regular_sale_price(driver: webdriver.Chrome):
-    sale_price = driver.find_element_by_tag_name('h4').find_element_by_tag_name(
-        'span').find_element_by_tag_name(
+    sale_price = driver.find_element_by_tag_name('h4').find_element_by_tag_name('span').find_element_by_tag_name(
         'b').find_element_by_tag_name('span').find_element_by_tag_name('span').text
     return Decimal(sale_price.replace('€', ''))
 
