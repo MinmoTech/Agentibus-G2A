fbb89b7716f1411d67f473e5a867feef09476c03
Julius Dehner <julius.dehner@yahoo.de>
2018-12-28 17:00:51 +0100

    Add catch for no title in fanaticalhandler

--- agentibus/StoreHandlers/FanaticalHandler.py
+++ agentibus/StoreHandlers/FanaticalHandler.py
@@ -11,16 +11,20 @@ from agentibus.Product import Game
 
 def set_game_data(driver, game: Game):
     driver.get(game.url)
-    game.name = driver.find_element_by_tag_name('h1').text
-    logging.getLogger().info(f'Getting {game.name} info.')
-    if driver.find_elements_by_class_name("stardeal-extras-container"):
-        game.sale_price = _get_stardeal_price(driver)
-    else:
-        try:
-            game.sale_price = _get_regular_sale_price(driver)
-        except NoSuchElementException:
-            logging.getLogger().error(f'Could not get proper info at {game.url}')
-    Product.set_game_meta_data(game, driver)
+    try:
+        logging.getLogger().info(f'Getting {game.name} info.')
+        game.name = driver.find_element_by_tag_name('h1').text
+
+        if driver.find_elements_by_class_name("stardeal-extras-container"):
+            game.sale_price = _get_stardeal_price(driver)
+        else:
+            try:
+                game.sale_price = _get_regular_sale_price(driver)
+            except NoSuchElementException:
+                logging.getLogger().error(f'Could not get proper info at {game.url}')
+        Product.set_game_meta_data(game, driver)
+    except NoSuchElementException:
+        logging.getLogger().error(f'Could not get game name at {game.url}')
 
 
 def _get_regular_sale_price(driver: webdriver.Chrome):
